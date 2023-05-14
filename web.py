a2bf5176327d0cfbedf20daeb2e03606b48ab93f
Ned Palacios <npdoesmc@gmail.com>
2023-05-15 02:16:01 +0800

    updated

--- web.py
+++ web.py
@@ -1,42 +1,47 @@
-import io
 import numpy as np
 from flask import Flask, render_template, request
 from tensorflow import keras
+import cv2
 
-ann_model = keras.models.load_model('models/mobile_receipt.h5')
+image_size = 224
+providers = ['gcash', 'paymaya']
+
+title = 'Mobile Wallet Receipt Classifier'
+description = 'Recognize if receipt is gcash or paymaya'
+
+models = [
+    keras.models.load_model('models/receipt_identify_stable1.h5'),
+    keras.models.load_model('models/receipt_identify_v2.h5'),
+    keras.models.load_model('models/receipt_identify_v3.h5'),
+    keras.models.load_model('models/receipt_identify_v4.h5')
+]
+    
+model_keys = ['stable1', 'stable2', 'v3', 'v4']
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index_page():
-    return render_template('index.html')
+    return render_template('index.html', title=title, description=description)
 
 @app.post("/prediction")
 def predict_inputs():
-    inputs = []
-    prediction = 'n/a'
+    # model_used = request.form.get('classifier')
+    input_file = request.files.get('input_file')
+    input_data = cv2.imdecode(np.frombuffer(input_file.read(), np.uint8), cv2.IMREAD_COLOR)
+    input_data = cv2.resize(input_data, (image_size, image_size))
+    final_input = np.expand_dims(input_data, axis=0)
+    raw_prediction = models[0].predict(final_input)
+    predictions = {}
 
-    input_file = request.files.get('input_file', None)
-    has_input_file = input_file is not None and input_file.content_type == 'text/csv'
-
-    if has_input_file:
-        input_file = request.files.get('input_file')
-        input_file.stream.seek(0)
-        input_data = io.StringIO(input_file.stream.read().decode("UTF8"))
-    else:
-        data = request.form
-        inputs = list(map(lambda name: [name, data.get(name)], feature_labels.keys()))
-
-    raw_inputs = list(map(lambda val: val[1], inputs))
-    inputs = list(map(lambda val: feature_labels_indices[val[0]][val[1]], inputs))
-
-    classifier = request.form.get('classifier')
-    final_input = np.expand_dims(np.array(inputs), axis=0)
-    raw_prediction = ann_model.predict(inputs)
+    for idx, name in enumerate(model_keys):
+        raw_prediction = models[idx].predict(final_input)
+        print(name, raw_prediction)
+        predictions[name] = providers[int(raw_prediction[0][0])]
 
     return {
-        'prediction': prediction
+        'predictions': predictions
     }
 
 if __name__ == "__main__":
